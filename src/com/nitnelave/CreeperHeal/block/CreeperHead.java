5f2dade13eb299d06a2e33f0daa9459fc9bc9b64
nitnelave <nitnelave1@gmail.com>
2013-01-10 18:58:48 +0100

    Fixed Skull replacement

--- src/com/nitnelave/CreeperHeal/block/CreeperHead.java
+++ src/com/nitnelave/CreeperHeal/block/CreeperHead.java
@@ -4,7 +4,7 @@ package com.nitnelave.CreeperHeal.block;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.BlockState;
-import org.bukkit.material.Skull;
+import org.bukkit.block.Skull;
 
 import com.nitnelave.CreeperHeal.utils.CreeperLog;
 
@@ -16,22 +16,20 @@ public class CreeperHead extends CreeperBlock {
 		CreeperLog.debug("skull");
 		World world = blockState.getWorld();
 		Location loc = getLocation();
-		
-		/*TileEntitySkull tileentityskull = (TileEntitySkull) world.getTileEntity(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
-
-        if (tileentityskull.getSkullType() == 3 && tileentityskull.getExtraType() != null && tileentityskull.getExtraType().length() > 0) {
-            itemstack.setTag(new NBTTagCompound());
-            itemstack.getTag().setString("SkullOwner", tileentityskull.getExtraType());
-        }
-
-        this.b(world, i, j, k, itemstack);*/
+		Skull skull = (Skull) blockState;
+		CreeperLog.debug(skull.getRotation().toString());
 	}
 	
 	@Override
 	public void update(boolean force) {
 		super.update(force);
 		Skull skull = (Skull) blockState;
-		((Skull) blockState.getBlock().getState()).setFacingDirection(skull.getFacing());
+		Skull newSkull = ((Skull) blockState.getBlock().getState());
+		newSkull.setRotation(skull.getRotation());
+		newSkull.setSkullType(skull.getSkullType());
+		if(skull.hasOwner())
+			newSkull.setOwner(skull.getOwner());
+		newSkull.update(force);
 	}
 
 }
