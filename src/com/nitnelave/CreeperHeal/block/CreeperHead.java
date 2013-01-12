1832d0e5ab3e9b848b2356f59575252820b0925d
nitnelave <nitnelave1@gmail.com>
2013-01-12 22:32:23 +0100

    Removed debugging messages.

--- src/com/nitnelave/CreeperHeal/block/CreeperHead.java
+++ src/com/nitnelave/CreeperHeal/block/CreeperHead.java
@@ -1,23 +1,13 @@
 package com.nitnelave.CreeperHeal.block;
 
 
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Skull;
 
-import com.nitnelave.CreeperHeal.utils.CreeperLog;
-
-@SuppressWarnings("unused")
 public class CreeperHead extends CreeperBlock {
 
 	protected CreeperHead(BlockState blockState) {
 		super(blockState);
-		CreeperLog.debug("skull");
-		World world = blockState.getWorld();
-		Location loc = getLocation();
-		Skull skull = (Skull) blockState;
-		CreeperLog.debug(skull.getRotation().toString());
 	}
 	
 	@Override
