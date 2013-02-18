901d44d1d6e9c63ff0a87e4bcacfd06e97efaf54
nitnelave <nitnelave1@gmail.com>
2013-02-18 01:04:18 +0100

    By correcting the AddTrapRunnable, the access to the explosion's blockList is no longer used, and is made strictly private by removing the getter.

--- src/main/java/com/nitnelave/CreeperHeal/utils/AddTrapRunnable.java
+++ src/main/java/com/nitnelave/CreeperHeal/utils/AddTrapRunnable.java
@@ -1,39 +1,33 @@
 package com.nitnelave.CreeperHeal.utils;
 
-import java.util.List;
-
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 
-import com.nitnelave.CreeperHeal.block.CreeperBlock;
 import com.nitnelave.CreeperHeal.block.CreeperExplosion;
-import com.nitnelave.CreeperHeal.block.Replaceable;
 
-public class AddTrapRunnable implements Runnable{
+public class AddTrapRunnable implements Runnable {
 
-	private CreeperExplosion cEx;
-	private Block block;
-	private Material type;
-	
-	public AddTrapRunnable(CreeperExplosion cEx, Block b, Material t)
-	{
-		this.cEx = cEx;
-		block = b;
-		type = t;
-	}
-	
-	@Override
-	public void run() {
-		BlockState tmp_state = block.getState();
+    private final CreeperExplosion cEx;
+    private final Block block;
+    private final Material type;
 
-		block.setType(type);                            //set the block to tnt
+    public AddTrapRunnable (CreeperExplosion cEx, Block b, Material t) {
+        this.cEx = cEx;
+        block = b;
+        type = t;
+    }
 
-		List<Replaceable> list = cEx.getBlockList();
-		list.add(CreeperBlock.newBlock(block.getState()));                //record it
+    @Override
+    public void run () {
+        BlockState tmp_state = block.getState ();
 
-		tmp_state.update(true);        //set it back to what it was
-		
-	}
+        block.setType (type); //set the block to tnt
+
+        cEx.record (block);
+
+        tmp_state.update (true); //set it back to what it was
+
+    }
 
 }
