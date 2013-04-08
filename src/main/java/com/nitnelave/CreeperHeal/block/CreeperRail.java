8d7519bfb5cccf10d35a358bf8bf4bea9c3564a3
nitnelave <nitnelave1@gmail.com>
2013-04-08 13:03:45 +0200

    Removed useless delaying in rails replacement.

--- src/main/java/com/nitnelave/CreeperHeal/block/CreeperRail.java
+++ src/main/java/com/nitnelave/CreeperHeal/block/CreeperRail.java
@@ -1,11 +1,8 @@
 package com.nitnelave.CreeperHeal.block;
 
-import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
-
-import com.nitnelave.CreeperHeal.CreeperHeal;
 
 /**
  * Rail implementation of CreeperBlock.
@@ -14,6 +11,8 @@ import com.nitnelave.CreeperHeal.CreeperHeal;
  * 
  */
 class CreeperRail extends CreeperBlock {
+    private static final BlockFace[] FACES = {BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH};
+    private static final BlockFace[] UP_DOWN = {BlockFace.DOWN, BlockFace.SELF, BlockFace.UP};
 
     /*
      * Constructor.
@@ -28,12 +27,26 @@ class CreeperRail extends CreeperBlock {
      */
     @Override
     public void update () {
-        Bukkit.getScheduler ().scheduleSyncDelayedTask (CreeperHeal.getInstance (), new Runnable () {
-            @Override
-            public void run () {
-                replaceRail ();
+
+        byte data = getRawData ();
+        byte[][] railData = new byte[3][4];
+        Block block = getBlock ();
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 4; j++)
+            {
+                Block tmpBlock = block.getRelative (FACES[j]);
+                tmpBlock = tmpBlock.getRelative (UP_DOWN[i]);
+                railData[i][j] = tmpBlock.getData ();
             }
-        });
+        blockState.update (true);
+        block.setData (data);
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 4; j++)
+            {
+                Block tmpBlock = block.getRelative (FACES[j]);
+                tmpBlock = tmpBlock.getRelative (UP_DOWN[i]);
+                tmpBlock.setData (railData[i][j]);
+            }
     }
 
     /*
@@ -55,40 +68,6 @@ class CreeperRail extends CreeperBlock {
             default:
                 return BlockFace.DOWN;
         }
-    }
-
-    /*
-     * Replace the rail and enforce the rails' direction, as it sometimes get
-     * messed up by the other rails around.
-     */
-    private void replaceRail () {
-        BlockFace[] faces = {BlockFace.EAST, BlockFace.WEST, BlockFace.NORTH, BlockFace.SOUTH};
-
-        byte data = getRawData ();
-        byte[][] railData = new byte[3][4];
-        Block block = getBlock ();
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 4; j++)
-            {
-                Block tmpBlock = block.getRelative (faces[j]);
-                if (i == 2)
-                    tmpBlock = tmpBlock.getRelative (BlockFace.UP);
-                else if (i == 0)
-                    tmpBlock = tmpBlock.getRelative (BlockFace.DOWN);
-                railData[i][j] = tmpBlock.getData ();
-            }
-        blockState.update (true);
-        block.setData (data);
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 4; j++)
-            {
-                Block tmpBlock = block.getRelative (faces[j]);
-                if (i == 2)
-                    tmpBlock = tmpBlock.getRelative (BlockFace.UP);
-                else if (i == 0)
-                    tmpBlock = tmpBlock.getRelative (BlockFace.DOWN);
-                tmpBlock.setData (railData[i][j]);
-            }
     }
 
     /**
