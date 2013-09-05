0537d8d10002201e43baa49af0d7bb4177f7b1f7
nitnelave <nitnelave1@gmail.com>
2013-09-05 18:34:23 +0200

    Factions compatibility for burnt blocks

--- src/main/java/com/nitnelave/CreeperHeal/listeners/CreeperBlockListener.java
+++ src/main/java/com/nitnelave/CreeperHeal/listeners/CreeperBlockListener.java
@@ -9,6 +9,7 @@ import com.nitnelave.CreeperHeal.block.BurntBlockManager;
 import com.nitnelave.CreeperHeal.config.CreeperConfig;
 import com.nitnelave.CreeperHeal.config.WCfgVal;
 import com.nitnelave.CreeperHeal.config.WorldConfig;
+import com.nitnelave.CreeperHeal.utils.FactionHandler;
 
 /**
  * Listener for all block-related events.
@@ -29,7 +30,7 @@ public class CreeperBlockListener implements Listener {
     public void onBlockBurn (BlockBurnEvent event) {
         WorldConfig world = CreeperConfig.getWorld (event.getBlock ().getLocation ().getWorld ());
 
-        if (world.getBool (WCfgVal.FIRE) && !world.isProtected (event.getBlock ()))
+        if (world.getBool (WCfgVal.FIRE) && !world.isProtected (event.getBlock ()) && !FactionHandler.shouldIgnore (event.getBlock (), world))
         {
             if (BurntBlockManager.wasRecentlyBurnt (event.getBlock ()))
             {
