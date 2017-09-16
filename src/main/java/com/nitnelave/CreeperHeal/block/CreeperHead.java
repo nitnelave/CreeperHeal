ebc5896efa6e497ca01a0368239ec4412153dbfa
Crepez <crepezmail@gmail.com>
2017-09-16 22:48:54 +0200

    fix: doesn't ignore the SkullMeta on drops of regenerated blocks anymore

--- src/main/java/com/nitnelave/CreeperHeal/block/CreeperHead.java
+++ src/main/java/com/nitnelave/CreeperHeal/block/CreeperHead.java
@@ -1,7 +1,13 @@
 package com.nitnelave.CreeperHeal.block;
 
+import com.nitnelave.CreeperHeal.config.CreeperConfig;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Skull;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 /**
  * Skull implementation of CreeperBlock, to store and replace the orientation,
@@ -37,6 +43,36 @@ class CreeperHead extends CreeperBlock
         if (skull.hasOwner())
             newSkull.setOwningPlayer(skull.getOwningPlayer());
         newSkull.update(true);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.nitnelave.CreeperHeal.block.CreeperBlock#drop(boolean)
+     */
+    @Override
+    public boolean drop(boolean forced)
+    {
+        if (forced || CreeperConfig.shouldDrop())
+        {
+            Location loc = blockState.getBlock().getLocation();
+            World w = loc.getWorld();
+
+            Skull skull = (Skull) blockState;
+            ItemStack s = new ItemStack(Material.SKULL_ITEM);
+            SkullMeta m = (SkullMeta) s.getItemMeta();
+            if (skull.hasOwner())
+                // Should be skull.getOwningPlayer().getName() instead of skull.getOwner();
+                // but somehow always returns null, so we need to use the deprecated method
+                m.setOwner(skull.getOwner());
+            s.setItemMeta(m);
+            s.setDurability((short) skull.getSkullType().ordinal());
+
+            w.dropItemNaturally(loc, s);
+
+            return true;
+        }
+        return false;
     }
 
 }
