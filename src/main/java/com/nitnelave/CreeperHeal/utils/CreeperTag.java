9a0df4b921f532ed3d88fff996132410d7afeb74
Jikoo <jikoo.games@gmail.com>
2019-02-17 12:32:02 -0500

    Update to 1.13.2

--- src/main/java/com/nitnelave/CreeperHeal/utils/CreeperTag.java
+++ src/main/java/com/nitnelave/CreeperHeal/utils/CreeperTag.java
@@ -1,6 +1,7 @@
 package com.nitnelave.CreeperHeal.utils;
 
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Tag;
 
 import java.util.Set;
@@ -14,36 +15,39 @@ public class CreeperTag implements Tag<Material>
 {
 
     public static final CreeperTag FLOWERS =
-            new CreeperTag(Material.DANDELION, Material.POPPY, Material.BLUE_ORCHID, Material.ALLIUM,
+            new CreeperTag("flowers", Material.DANDELION, Material.POPPY, Material.BLUE_ORCHID, Material.ALLIUM,
                     Material.AZURE_BLUET, Material.RED_TULIP, Material.ORANGE_TULIP, Material.WHITE_TULIP,
                     Material.PINK_TULIP, Material.OXEYE_DAISY);
 
     public static final CreeperTag INFESTED_BLOCKS =
-            new CreeperTag(Material.INFESTED_CHISELED_STONE_BRICKS, Material.INFESTED_COBBLESTONE,
+            new CreeperTag("infested_blocks", Material.INFESTED_CHISELED_STONE_BRICKS, Material.INFESTED_COBBLESTONE,
                     Material.INFESTED_CRACKED_STONE_BRICKS, Material.INFESTED_MOSSY_STONE_BRICKS,
                     Material.INFESTED_STONE, Material.INFESTED_STONE_BRICKS);
 
     public static final CreeperTag STANDING_BANNERS =
-            new CreeperTag(Material.BLACK_BANNER, Material.BLUE_BANNER, Material.BROWN_BANNER, Material.CYAN_BANNER,
+            new CreeperTag("standing_banners", Material.BLACK_BANNER, Material.BLUE_BANNER, Material.BROWN_BANNER, Material.CYAN_BANNER,
                     Material.GRAY_BANNER, Material.GREEN_BANNER, Material.LIGHT_BLUE_BANNER, Material.LIGHT_GRAY_BANNER,
                     Material.LIME_BANNER, Material.MAGENTA_BANNER, Material.ORANGE_BANNER, Material.PINK_BANNER,
                     Material.PURPLE_BANNER, Material.RED_BANNER, Material.WHITE_BANNER, Material.YELLOW_BANNER);
 
     public static final CreeperTag WALL_BANNERS =
-            new CreeperTag(Material.BLACK_WALL_BANNER, Material.BLUE_WALL_BANNER, Material.BROWN_WALL_BANNER, Material.CYAN_WALL_BANNER,
+            new CreeperTag("wall_banners", Material.BLACK_WALL_BANNER, Material.BLUE_WALL_BANNER, Material.BROWN_WALL_BANNER, Material.CYAN_WALL_BANNER,
                     Material.GRAY_WALL_BANNER, Material.GREEN_WALL_BANNER, Material.LIGHT_BLUE_WALL_BANNER, Material.LIGHT_GRAY_WALL_BANNER,
                     Material.LIME_WALL_BANNER, Material.MAGENTA_WALL_BANNER, Material.ORANGE_WALL_BANNER, Material.PINK_WALL_BANNER,
                     Material.PURPLE_WALL_BANNER, Material.RED_WALL_BANNER, Material.WHITE_WALL_BANNER, Material.YELLOW_WALL_BANNER);
 
     public static final CreeperTag DOUBLE_FLOWERS =
-            new CreeperTag(Material.SUNFLOWER, Material.LILAC, Material.ROSE_BUSH, Material.PEONY);
+            new CreeperTag("double_flowers", Material.SUNFLOWER, Material.LILAC, Material.ROSE_BUSH, Material.PEONY);
 
+    private final NamespacedKey key;
     private final Set<Material> tagged;
 
-    private CreeperTag(Material... elements)
+    private CreeperTag(String key, Material... elements)
     {
+        this.key = new NamespacedKey("creeperheal", key);
         tagged = CreeperUtils.createFinalHashSet(elements);
     }
+
     @Override
     public boolean isTagged(Material keyed)
     {
@@ -56,4 +60,9 @@ public class CreeperTag implements Tag<Material>
         return tagged;
     }
 
+    @Override
+    public NamespacedKey getKey()
+    {
+        return key;
+    }
 }
